be624a4a09261e4542018e8fc997c617e86c9397
alima08 <41311324+alima08@users.noreply.github.com>
2019-09-07 18:51:39 +0700

    Create Nomor1.js

---
+++ Nomor1.js
@@ -0,0 +1,39 @@
+<script>
+	var biodata = {
+	"name" : "Alima Rasyida Amin",
+	"age"  : 21,
+	"address" : "Batang, Central Java",
+	"hobbies" : ["googling", "doodling" ],
+	"is_married" : false,
+	"list_school" : [
+    	{
+        	"name" : "institut teknologi sepuluh nopember",
+        	"year_in" : 2015,
+        	"year_out" : 2019,
+        	"major" : "mathematics"
+    	},
+    	{
+        	"name" : "sma 3 pekalongan",
+        	"year_in" : 2012,
+        	"year_out" : 2015,
+        	"major" : "science"
+    	}
+	],
+   	 
+	"skill" : [
+    	{
+        	"skill_name" : "programming",
+        	"level" : "beginer"
+    	},
+    	{
+        	"skill_name" : "graphic design",
+        	"level" : "beginner"
+    	}
+	],
+    
+	"interest_in_coding" : true
+}
+
+alert(JSON.stringify(biodata));
+
+</script>
