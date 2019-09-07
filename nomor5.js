4826ce768c50e6d4186eebccfec9a4076888975b
alima08 <41311324+alima08@users.noreply.github.com>
2019-09-07 23:00:14 +0700

    Create nomor5.js

---
+++ nomor5.js
@@ -0,0 +1,15 @@
+<script>
+
+function groupNumber(angka){
+    var ar = angka.split("");
+    var num;
+    for (var i = 0; i<ar.length;i++){
+        if (Number.isInteger(ar(i))==true){
+            num
+        }
+    }
+    return ar;
+}
+
+document.write(groupNumber("993141 -1 1323 14-232"));
+</script>
